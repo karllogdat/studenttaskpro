49f1f7e5a51baf6e9e521736b642b62de2e3ceaf
karllogdat <logdatk@gmail.com>
2025-01-09 15:52:22 +0800

    add initial serialization for TodoList

--- src/main/java/com/hexacode/studenttaskpro/TodoList.java
+++ src/main/java/com/hexacode/studenttaskpro/TodoList.java
@@ -1,9 +1,32 @@
 package com.hexacode.studenttaskpro;
 
+import java.io.*;
 import java.util.PriorityQueue;
 
 public class TodoList {
-    private PriorityQueue<TodoEntry> list;
+    private PriorityQueue<TodoEntry> list = new PriorityQueue<TodoEntry>();
+    private final File file = new File("src/main/resources/com/hexacode/studenttaskpro/todos.ser");
 
-    // void rearrange()
+    TodoList() {
+        readTodosFromFile();
+    }
+
+    public void readTodosFromFile() {
+        try {
+            FileInputStream fileIn = new FileInputStream(file);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+
+            // file is empty, no need to read object
+            if (file.length() == 0) {
+                return;
+            }
+
+        } catch (IOException _) {
+            System.out.println("No file found: " + file.getPath());
+        }
+    }
+
+    public void writeTodosToFile() {
+        // TODO: unimplemented
+    }
 }
